a4631281fb5206ad188ed6d537d7c1a85a673ee3
dimazhylko <zhylko.dima@gmail.com>
2018-11-29 14:33:24 +0100

    bug fix v2

--- Z7/cw1.cpp
+++ Z7/cw1.cpp
@@ -12,12 +12,12 @@ int c=0;
 bool check(int row, int col){
 	if(row>=5 || row<0 || col>=5 || col<0 || P[row][col]!=0)return false;
 	c++;
-	if(c==25)return true;
-
 	P[row][col]=c;
+	if(c==25)return true;
 
 	for(int i = 1;i<=2;i++){
 		for(int j=2;j>=1;j--){
+			if(i==1 && j==1)continue;
 			if(check(row+i, col+j) ||
 				check(row+i, col-j) ||
 				check(row-i, col+j) ||
@@ -44,5 +44,11 @@ int main(){
 		cout<<"No"<<endl;
 	}
 
+	for(int i = 0;i<5;i++){
+		for(int j=0;j<5;j++){
+			cout<<P[i][j]<<" ";
+		}
+		cout<<endl;
+	}
 	return 0;
 }
